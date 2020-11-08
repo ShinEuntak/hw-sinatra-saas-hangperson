d8883c08853c88f89ff70474b0a31e9444dcfb0c
Shin Euntak <et9129@naver.com>
2020-11-08 14:14:27 +0000

    homework

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,57 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+ 
+  attr_accessor :word,:guesses,:wrong_guesses
+ 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    
+  end
+  
+  def guess(letter)
+    @letter = letter
+    raise ArgumentError if @letter.nil? || @letter.empty? || @letter =~ /[^A-z]/
+    if @letter =~ /[a-z]/
+      if !(@guesses.include? @letter) && !(@wrong_guesses.include? @letter)
+        if @word.include? @letter 
+          @guesses << @letter
+        else
+          @wrong_guesses << @letter
+        end
+      end
+      return true
+    else
+      return false
+    end
+  end
+ 
+ 
+  def word_with_guesses()
+    rword = @word
+    rword.chars{
+      |x| if !(@guesses.include? x)
+        rword.gsub!(x,"-")
+      end
+    }
+    return rword
+  end
+ 
+  def check_win_or_lose()
+    count = 0
+    @word.chars {|x|
+      if @guesses.include? x
+        count += 1
+      end
+    }
+    if count == @word.length
+      return :win
+    elsif @wrong_guesses.length == 7
+      return :lose
+    else return :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
